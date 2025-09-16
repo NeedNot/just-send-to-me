91f2ea26219e4116d042fbbdebd766d07c88fab7
Need_Not <47156725+NeedNot@users.noreply.github.com>
2025-09-16 16:35:51 -0700

    sign up revamp

--- src/features/auth/components/sign-up-form.tsx
+++ src/features/auth/components/sign-up-form.tsx
@@ -1,25 +1,22 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { cn } from '@/lib/utils';
 import { useState } from 'react';
 import { useSignUp } from '../api/sign-up';
 import { toast } from 'sonner';
 import { useRouter } from '@tanstack/react-router';
+import { ArrowLeft } from 'lucide-react';
 
 export function SignUpForm() {
+  const [showEmailSignUpForm, setShowEmailSignUpForm] = useState(false);
+  const [emailInput, setEmailInput] = useState('');
   const router = useRouter();
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
-  };
 
   const signUp = useSignUp({
     onError: (error: Error) => {
@@ -32,17 +29,19 @@ export function SignUpForm() {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (!showEmailSignUpForm) {
+      setShowEmailSignUpForm(true);
+      return;
+    }
+
     const formData = new FormData(event.currentTarget);
     const data = Object.fromEntries(formData.entries());
 
-    const { name, email, password, confirmPassword } = data as {
+    const { name, email, password } = data as {
       name: string;
       email: string;
       password: string;
-      confirmPassword: string;
     };
-
-    if (confirmPassword !== password) return;
 
     signUp.mutate({ name, email, password });
   };
@@ -50,129 +49,45 @@ export function SignUpForm() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Sign up</CardTitle>
+        <CardTitle>Create account</CardTitle>
+        {showEmailSignUpForm && (
+          <CardDescription>
+            <a
+              href="#"
+              onClick={() => setShowEmailSignUpForm(false)}
+              className="flex items-center gap-1"
+            >
+              <ArrowLeft size={16} /> Go back
+            </a>
+          </CardDescription>
+        )}
       </CardHeader>
       <CardContent>
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-4">
-              <Button
-                variant="outline"
-                className="w-full"
-                onClick={(e) => e.preventDefault()}
-              >
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Google</title>
-                  <path
-                    fill="currentColor"
-                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                  />
-                </svg>
-                Continue with Google
-              </Button>
-
-              <Button
-                variant="outline"
-                className="w-full"
-                onClick={(e) => e.preventDefault()}
-              >
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Meta</title>
-                  <path
-                    fill="currentColor"
-                    d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
-                  />
-                </svg>
-                Continue with Meta
-              </Button>
-            </div>
-            <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-              <span className="bg-card text-muted-foreground relative z-10 px-2">
-                Or continue with
-              </span>
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="m@example.com"
-                value={form.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div
-              className={cn(
-                'grid transition-all duration-250',
-                !!form.email ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]',
-              )}
-            >
-              <div className="overflow-hidden focus-within:overflow-visible">
-                <div className="mb-6 grid gap-3">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" name="name" type="text" required />
+            {showEmailSignUpForm ? (
+              <EmailSignUpForm value={emailInput} onChange={setEmailInput} />
+            ) : (
+              <>
+                <ContinueWithGoogle />
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
+                    Or continue with
+                  </span>
                 </div>
-                <div className="mb-6 grid gap-3">
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    value={form.password}
-                    onChange={handleChange}
-                    minLength={8}
-                    maxLength={128}
-                    required
-                  />
-                  {form.password &&
-                    form.confirmPassword &&
-                    form.password !== form.confirmPassword && (
-                      <p className="text-sm text-red-400">
-                        Passwords do not match
-                      </p>
-                    )}
-                </div>
-                <div className="mb-6 grid gap-3">
-                  <Label htmlFor="confirm-password">Password</Label>
-                  <Input
-                    id="confirm-password"
-                    name="confirmPassword"
-                    type="password"
-                    value={form.confirmPassword}
-                    onChange={handleChange}
-                    minLength={8}
-                    maxLength={128}
-                    required
-                  />
-                  {form.password &&
-                    form.confirmPassword &&
-                    form.password !== form.confirmPassword && (
-                      <p className="text-sm text-red-400">
-                        Passwords do not match
-                      </p>
-                    )}
-                </div>
-                <div className="flex flex-col gap-3">
-                  <Button
-                    type="submit"
-                    className="w-full"
-                    disabled={signUp.isPending}
-                  >
-                    Sign Up
-                  </Button>
-                </div>
-              </div>
-            </div>
+                <Input
+                  autoComplete="off"
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  value={emailInput}
+                  onChange={(e) => setEmailInput(e.currentTarget.value)}
+                  required
+                />
+              </>
+            )}
+            <Button>Continue with email</Button>
           </div>
           <div className="mt-4 text-center text-sm">
             Already have an account?{' '}
@@ -183,5 +98,68 @@ export function SignUpForm() {
         </form>
       </CardContent>
     </Card>
+  );
+}
+
+function ContinueWithGoogle() {
+  return (
+    <div className="flex flex-col gap-4">
+      <Button
+        variant="outline"
+        className="w-full"
+        onClick={(e) => e.preventDefault()}
+      >
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <title>Google</title>
+          <path
+            fill="currentColor"
+            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+          />
+        </svg>
+        Continue with Google
+      </Button>
+    </div>
+  );
+}
+
+function EmailSignUpForm({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+}) {
+  return (
+    <>
+      <Input
+        key={'abc'}
+        id="email-2"
+        autoComplete="off"
+        name="email"
+        type="email"
+        placeholder="Email"
+        value={value}
+        onChange={(e) => onChange(e.currentTarget.value)}
+        required
+      />
+      <Input
+        type="text"
+        name="name"
+        id="name"
+        minLength={2}
+        placeholder="Name"
+        autoComplete="name"
+        required
+      />
+      <Input
+        id="password"
+        name="password"
+        type="password"
+        minLength={8}
+        autoComplete="new-password"
+        placeholder="Password (At least 8 characters)"
+        required
+      />
+    </>
   );
 }
