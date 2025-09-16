49cd5f69d9337df0c9d6eb34b03864df522abbdb
Need_Not <47156725+NeedNot@users.noreply.github.com>
2025-09-16 10:18:31 -0700

    fixed flash when switching pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from './routeTree.gen';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 
