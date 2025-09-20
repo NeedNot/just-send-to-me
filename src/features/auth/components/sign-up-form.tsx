ccdf24735479ad3e9f5c23f40a4c8ff463b905fd
Need_Not <47156725+NeedNot@users.noreply.github.com>
2025-09-20 10:49:19 -0700

    Sign in with google on sign in

--- src/features/auth/components/sign-up-form.tsx
+++ src/features/auth/components/sign-up-form.tsx
@@ -12,7 +12,7 @@ import { useSignUp } from '../api/sign-up';
 import { toast } from 'sonner';
 import { useRouter } from '@tanstack/react-router';
 import { ArrowLeft } from 'lucide-react';
-import { authClient } from '@/lib/better-auth';
+import { ContinueWithGoogle } from './social-sign-in';
 
 export function SignUpForm({ ...props }: React.ComponentProps<typeof Card>) {
   const [showEmailSignUpForm, setShowEmailSignUpForm] = useState(false);
@@ -46,38 +46,6 @@ export function SignUpForm({ ...props }: React.ComponentProps<typeof Card>) {
 
     signUp.mutate({ name, email, password });
   };
-
-  const handleLoginWithGoogle = () => {
-    authClient.signIn.social({
-      provider: 'google',
-    });
-  };
-
-  function ContinueWithGoogle() {
-    return (
-      <div className="flex flex-col gap-4">
-        <Button
-          variant="outline"
-          type="button"
-          className="w-full"
-          onClick={handleLoginWithGoogle}
-        >
-          <svg
-            role="img"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Google</title>
-            <path
-              fill="currentColor"
-              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-            />
-          </svg>
-          Continue with Google
-        </Button>
-      </div>
-    );
-  }
 
   return (
     <Card {...props}>
@@ -120,7 +88,9 @@ export function SignUpForm({ ...props }: React.ComponentProps<typeof Card>) {
                 />
               </>
             )}
-            <Button disabled={signUp.isPending}>Continue with email</Button>
+            <Button disabled={signUp.isPending}>
+              {!showEmailSignUpForm ? 'Continue with email' : 'Sign Up'}
+            </Button>
           </div>
           <div className="mt-4 text-center text-sm">
             Already have an account?{' '}
@@ -144,7 +114,6 @@ function EmailSignUpForm({
   return (
     <>
       <Input
-        key={'abc'}
         id="email-2"
         autoComplete="off"
         name="email"
